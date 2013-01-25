349d4c5efb85cd080d156d8e4f3fcc0807b7f81f
mikeda <baguzy@gmail.com>
2013-01-26 06:11:17 +0900

    init rails : rails new sabakan --skip-test-unit --skip-bundle --database=mysql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sabakan::Application.config.secret_token = 'c23ce048dc1b3aa39b364d15be3ed6cf14743b75b9d9d3cd53c0f98b76b866776aa3af818ecf8cf052504d84e611a25689a6e7ec6884bcf08f3608462593cbf4'

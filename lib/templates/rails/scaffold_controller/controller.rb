160e87ddcd156d27beefb216bd423416ac89992f
mikeda <baguzy@gmail.com>
2013-01-28 02:22:25 +0900

    modify "scaffold_controller" template to use respond_with()

---
+++ lib/templates/rails/scaffold_controller/controller.rb
@@ -0,0 +1,51 @@
+<% if namespaced? -%> 
+require_dependency "<%= namespaced_file_path %>/application_controller"
+
+<% end -%>
+<% module_namespacing do -%>
+class <%= controller_class_name %>Controller < ApplicationController
+  respond_to :html
+
+  def index
+    @<%= plural_table_name %> = <%= orm_class.all(class_name) %>
+    respond_with @<%= plural_table_name %>
+  end
+
+  def show
+    @<%= singular_table_name %> = <%= orm_class.find(class_name, "params[:id]") %>
+    respond_with @<%= singular_table_name %>
+  end
+
+  def new
+    @<%= singular_table_name %> = <%= orm_class.build(class_name) %>
+    respond_with @<%= singular_table_name %>
+  end
+
+  def edit
+    @<%= singular_table_name %> = <%= orm_class.find(class_name, "params[:id]") %>
+    respond_with @<%= singular_table_name %>
+  end
+
+  def create
+    @<%= singular_table_name %> = <%= orm_class.build(class_name, "params[:#{singular_table_name}]") %>
+    if @<%= orm_instance.save %>
+      flash[:notice] = "<%= human_name %> successfully created."
+    end
+    respond_with @<%= singular_table_name %>
+  end
+
+  def update
+    @<%= singular_table_name %> = <%= orm_class.find(class_name, "params[:id]") %>
+    if @<%= orm_instance.update_attributes("params[:#{singular_table_name}]") %>
+      flash[:notice] = "<%= human_name %> successfully updated."
+    end
+    respond_with @<%= singular_table_name %>
+  end
+
+  def destroy
+    @<%= singular_table_name %> = <%= orm_class.find(class_name, "params[:id]") %>
+    @<%= orm_instance.destroy %>
+    respond_with @<%= singular_table_name %>
+  end
+end
+<% end -%>
